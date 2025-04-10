cf5fb3cf1c8f29e35e9cc6689864012851e384f7
Naser Zia Neiazi <naser123neyazi@gmail.com>
2025-04-10 11:14:07 +0430

    Fresh Creation of Project

---
+++ local_donation_finder/src/main/java/com/example/local_donation_finder/LocalDonationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_donation_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDonationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDonationFinderApplication.class, args);
+	}
+
+}
---
+++ local_donation_finder/src/test/java/com/example/local_donation_finder/LocalDonationFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.local_donation_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDonationFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
